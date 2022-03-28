76de131cf801cc3b752a012606cb35c337e8da7d
g1ll from d3567 <gillvelleda@gmail.com>
2022-03-28 15:50:41 -0300

    Exemplo de seleção de um intervalo com o AND

--- app.js
+++ app.js
@@ -61,15 +61,43 @@ try {
     //             }).toArray()
 
     //Exemplo de filtro de dados
-    const resultados = await client.db('loja').collection('produtos')
-        .find({
-                preco:{$lt:15000},
-                importado:{$eq:true}
-            },
-            {   
-                sort:{preco:-1},
-                projection: { _id: 0,qtd_estoque: 0, descricao: 0}
-            }).toArray()
+    // const resultados = await client.db('loja').collection('produtos')
+    //     .find({
+    //             preco:{$lt:15000},
+    //             importado:{$eq:true}
+    //         },
+    //         {   
+    //             sort:{preco:-1},
+    //             projection: { _id: 0,qtd_estoque: 0, descricao: 0}
+    //         }).toArray()
+
+    //Exemplo de operadores de comparação
+    // const filtro = {
+    //     importado:{$eq:false},//produtos nacionais
+    //     qtd_estoque:{$gte:200}//com 200 ou mais itens em estoque
+    // }
+    // const opcoes = {   
+    //     sort:{qtd_estoque:1},
+    //     projection: { _id: 0,preco: 0, descricao: 0}
+    // }
+
+    // //Exemplo de filtro com in ou nin
+    // const filtro = {
+    //     id_prod:{$nin:[111,115,125,124]}
+    // }
+
+    //OPERADORES LÓGICOS
+    //AND
+    const filtro = {
+            $and:[
+                {preco:{$gte:3000}},
+                {preco:{$lte:9000}}
+            ]
+        }
+    const opcoes ={sort:{preco:1},projection: { _id: 0, descricao: 0}}
+
+    const collection = client.db('loja').collection('produtos')
+    const resultados = await collection.find(filtro,opcoes).toArray()
     console.table(resultados)
 
 } catch (error) {
